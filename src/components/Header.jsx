4329b1bf8172f9c4fc66e06af41231e83e0bec32
GioMogi <giovannimogi@cynthias-mbp.lan>
2024-05-24 18:03:13 -0700

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+      <h1 className="text-xl md:text-4xl front-semibold tracking-widest text-center text-amber-800">Investment Calculator</h1>
+    </header>
+  );
+}
